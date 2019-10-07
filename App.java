6e3bb7158fde4ccaa4c0d03229f8a2d69e1f6d32
ahsanuddin786 <55526909+ahsanuddin786@users.noreply.github.com>
2019-10-07 03:53:08 -0500

    modified mappings

---
+++ App.java
@@ -0,0 +1,10 @@
+package com.musixmatch.spring;
+
+import org.springframework.web.client.RestTemplate;
+
+public class App {
+    public RestTemplate restTemplate(){
+        System.out.println( "Test" );
+		return null;
+    }
+}
